40ec8fc40ab1cec98feb45e33548c4926e2d9e78
Cardoso <lsc.cardosoo@gmail.com>
2020-10-27 22:59:20 -0300

    Updates

---
+++ precache-manifest.f5fc13d6d61955c5b7a39fface6cbc76.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65e01fd5c38a49036f1320eb71e1a8a",
+    "url": "/sd-03-trybeer-4/index.html"
+  },
+  {
+    "revision": "2a3998144177fdd32c4a",
+    "url": "/sd-03-trybeer-4/static/css/main.d27b784f.chunk.css"
+  },
+  {
+    "revision": "93f8b76271563e8bb7db",
+    "url": "/sd-03-trybeer-4/static/js/2.78bf7c3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sd-03-trybeer-4/static/js/2.78bf7c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3998144177fdd32c4a",
+    "url": "/sd-03-trybeer-4/static/js/main.a3c20775.chunk.js"
+  },
+  {
+    "revision": "abfff464741f3cbe996f",
+    "url": "/sd-03-trybeer-4/static/js/runtime-main.38ce141d.js"
+  },
+  {
+    "revision": "442dc3275f97052d4dffadb5f6fd77dd",
+    "url": "/sd-03-trybeer-4/static/media/Antarctica Pilsen 300ml.442dc327.jpg"
+  },
+  {
+    "revision": "f6531ee0b01c0d2fda93684fbcb4b129",
+    "url": "/sd-03-trybeer-4/static/media/Brahma 600ml.f6531ee0.jpg"
+  },
+  {
+    "revision": "fe16cc3f00fce99db3c4bf8dc6feb141",
+    "url": "/sd-03-trybeer-4/static/media/Heineken 600ml.fe16cc3f.jpg"
+  },
+  {
+    "revision": "4a08a7275b7c71d6e8718b7e9bdcb4c9",
+    "url": "/sd-03-trybeer-4/static/media/Skol 269ml.4a08a727.jpg"
+  },
+  {
+    "revision": "8c5ebf5bd4bbcdc3d01069f954d3bf1c",
+    "url": "/sd-03-trybeer-4/static/media/Skol Beats Senses 269ml.8c5ebf5b.jpg"
+  },
+  {
+    "revision": "9fb748e3026204530fe98c8900f63c8c",
+    "url": "/sd-03-trybeer-4/static/media/Skol Beats Senses 313ml.9fb748e3.jpg"
+  },
+  {
+    "revision": "c43788d3870f893c7adc77ad72861d51",
+    "url": "/sd-03-trybeer-4/static/media/Skol Lata 350ml.c43788d3.jpg"
+  },
+  {
+    "revision": "bcb9cd6ae32215694e862704fb5d353e",
+    "url": "/sd-03-trybeer-4/static/media/Stella Artois 275ml.bcb9cd6a.jpg"
+  },
+  {
+    "revision": "47c8dfcd3e6bb99c29478ed3c879a1d7",
+    "url": "/sd-03-trybeer-4/static/media/bg-card-orders.47c8dfcd.png"
+  },
+  {
+    "revision": "49d05f586c57cb6e7730f8ab5f64a896",
+    "url": "/sd-03-trybeer-4/static/media/delivery.49d05f58.png"
+  },
+  {
+    "revision": "eaeafd7c94c7728a2b9e4cd529dfe203",
+    "url": "/sd-03-trybeer-4/static/media/facebook.eaeafd7c.png"
+  },
+  {
+    "revision": "7453ea81bcfbb5547964c73da7b67f38",
+    "url": "/sd-03-trybeer-4/static/media/friends.7453ea81.png"
+  },
+  {
+    "revision": "129085e293dd0d571d4b600ec4441144",
+    "url": "/sd-03-trybeer-4/static/media/instagram.129085e2.png"
+  },
+  {
+    "revision": "1907717c8d618bbafbee47b4326fad83",
+    "url": "/sd-03-trybeer-4/static/media/whatsapp.1907717c.png"
+  }
+]);
